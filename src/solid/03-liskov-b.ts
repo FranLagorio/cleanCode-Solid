03f0d1c50d5eb9dba32e32ed0bfc7a51e1bd6995
franlagorio <franlagorio@gmail.com>
2023-06-26 11:46:58 -0300

    update

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,15 +1,29 @@
 export abstract class Vehicle {
+  protected altura: number;
+  protected peso: number;
+  constructor(altura: number, peso: number) {
+    this.altura = altura;
+    this.peso = peso;
+  }
+
+  get getAltura() {
+    return this.altura;
+  }
   //  getNumberOfSeats(): number {
   //     throw Error("Method not implemented");
   //   }
   abstract getNumerOfSeats(): number;
 }
-
+// extender de una clase abstracta frente  ala ventaja de imple,entar interface
+// es que podes aceder a variables de la clase asbtracta
 //herrera lo hizo con extends entonces tenias que poner el
 //super()
-export class Tesla implements Vehicle {
-  constructor(private numberOfSeats: number) {}
+export class Tesla extends Vehicle {
+  constructor(private numberOfSeats: number, altura: number, peso: number) {
+    super(altura, peso);
+  }
   getNumerOfSeats(): number {
+    this.getAltura;
     return this.numberOfSeats;
   }
 }
